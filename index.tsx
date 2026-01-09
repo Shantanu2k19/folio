5758d0b5bcda9e2b368de488f21c8fbf3f9fb74c
shantanu2k19 <shantanusingh2030@gmail.com>
2026-01-09 09:29:20 +0530

    chnages

--- index.tsx
+++ index.tsx
@@ -424,7 +424,7 @@ const App = () => {
             </section>
 
             <footer className="max-w-md pb-16 text-sm text-slate-500">
-              <p>Designed in the spirit of Brittany Chiang. Built with React and Tailwind CSS.</p>
+              <p>Built with React and Tailwind CSS.</p>
             </footer>
           </main>
         </div>
